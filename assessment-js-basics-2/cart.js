5a449c6c2702e66f4b337c7db2eb95a82ce1afdc
Ha Bui <ha.hoangbui97@gmail.com>
2022-07-05 15:10:52 -0500

    not finished

---
+++ assessment-js-basics-2/cart.js
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+// const cart = [
+//     {
+//         name: 'pizza', 
+//         price: 9.99
+//     }, 
+//     {
+//         name: 'pasta', 
+//         price: 8.99
+//     }, 
+//     {
+//         name: 'salad', 
+//         price: 7.99
+//     }
+// ]
+
+//CODE HERE
+const cart = [
+    {name: 'pizza',price: 9.99},
+    {name: 'pasta',price: 8.99},
+    {name: 'salad',price: 7.99}
+    ];
+    
+    const sum = cart.reduce((temp, x) => { 
+      return temp + x.price;
+    }, 0);
+    
+    console.log(sum);
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+function calcFinalPrice(cartTotal, couponValue, tax) {
+    const taxAmount = cartTotal * tax;
+    const totalWithTax = cartTotal + taxAmount;
+    const finalTotal = totalWithTax - couponValue;
+    return finalTotal;
+  }
+
+  console.log(calcFinalPrice(100, 10, .06)); 
+
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+   The properties of my customer object are: name, address, phone number, and email. 
+   I chose these properties because they are the most basic pieces of information that a restaurant would need about its customers. 
+   Name and address should be strings, because they are both text values. 
+   Phone number should be a number, because it is a numerical value. Email should be a string, because it is a text value.
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+
